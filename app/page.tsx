0e28eff95f021455cbc3a351bf8d608bde066a82
nate-hunter <hunter.j.nate@gmail.com>
2025-11-08 05:44:53 -0500

    refactored homepage to include theme fonts and updated title to "Prototype Composer"

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import styles from './styles/home.module.css';
-import { instrumentSans } from './fonts';
+import { ibmPlexMono, orbitron } from './fonts';
 
 export default function Home() {
   // Add your prototypes to this array
@@ -24,9 +24,9 @@ export default function Home() {
   ];
 
   return (
-    <div className={`${styles.container} ${instrumentSans.className}`}>
+    <div className={styles.container}>
       <header className={styles.header}>
-        <h1>Nate's prototypes</h1>
+        <h1>Prototype Composer</h1>
       </header>
 
       <main>
